5f4b2eb0cb987f668d92953e4f98cdc075ddef2b
Jaren Schnoebelen <jarenschnoebelen@yahoo.com>
2021-07-27 21:43:28 -0500

    added if else statements

--- app.js
+++ app.js
@@ -15,8 +15,58 @@ let lowerCaseName = userName.toLowerCase();
 console.log(lowerCaseName);
 
 //Validate the input using conditional logic using strict equals.
-if(lowerCaseName === 'craig' || lowerCaseName === 'bob') {
-  alert('Welcom back to my page, ' + userName);
+if(lowerCaseName === userName) {
+  alert('Welcome back to my page, ' + userName);
 } else {
   alert('Welcome new user to my page.');
+}
+
+let myTeam = prompt('What is my favorite team?');
+console.log(myTeam);
+
+//Validate answer is correct
+if(myTeam === 'cubs') {
+    alert('Correct, nice job, ' + userName);
+} else {
+    alert('Sorry, incorrect');
+}
+
+//prompt our user for input data
+let mySport = prompt('What is my favorite sport?');
+console.log(mySport);
+
+if(mySport === 'baseball') {
+    alert('Hey, that is correct!');
+} else {
+    alert('That is not my favorite, but I like that too.');
+}
+
+//prompt our user for input data
+let mySeason = prompt('What is my favorite season?');
+console.log(mySeason);
+
+if(mySeason === 'summer') {
+    alert('Yes! I love summer!');
+} else {
+    alert('Nope. Not a fan.');
+}
+
+//prompt our user for input data
+let mySteak = prompt('How do I like my steak?');
+console.log(mySteak);
+
+if(mySteak === 'medium rare') {
+    alert('Of course. Medium rare is the best!');
+} else {
+    alert('Nope. Not a fan.');
+}
+
+//prompt our user for input data
+let myMusic = prompt('Do I prefer country or rap music?');
+console.log(myMusic);
+
+if(myMusic === 'rap') {
+    alert('Yup! But i like country too.');
+} else {
+    alert('Nope. But I like country music sometimes.');
 }
